41f7c65572e055c0c5ae24efe8eac1578ff74cc5
montekaka <josh.chen.jia@gmail.com>
2021-09-20 23:51:49 -0700

    adds ant component to qr image and button

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import {Home, Chatroom} from './screens'
 import {Navbar} from './components'
 import { Layout} from 'antd';
-const { Content } = Layout;
+const { Content, Footer} = Layout;
 
 function App() {
   return (
     <Router>
       <Layout className="layout">
         <Navbar/>
-        <Content style={{ padding: '0 50px' }}>          
+        <Content  className="site-layout" style={{ padding: '50px 50px'}}>          
           <Switch>
             <Route exact path="/"><Home/></Route>
             <Route exact path="/chatroom"><Chatroom/></Route>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,25 +1,35 @@
 import React, {useEffect, useState} from 'react';
+import { Button, Image, Space } from 'antd';
 import wechat from '../apis/wechat'
 
 const Home = () => {
 
   const [qrLink, setQrLink] = useState('')
+  const [loading, setLoading] = useState(false)
 
   const handleGetQR = () => {
+    setLoading(true)
     wechat.get(`/v1/get_tmp_qr_code_url?sceneId=1000&expire=180`)
     .then((res) => {
+      console.log(res.data.code)
       setQrLink(res.data.code)
+      setLoading(false)
     })
     .catch((err) => {
       console.log(err)
+      setLoading(false)
     })
   }
 
   return (
-    <div>
-      <h1>Create a QR code</h1>
-      <div onClick={handleGetQR}>Click</div>
-      {qrLink && <img src={qrLink}/>}
+    <div style={{display: 'flex',
+      flexDirection: 'column', 
+      alignItems: 'center'
+    }}>
+      <h1>Welcome</h1>
+      <Button onClick={handleGetQR} type="primary" loading={loading}>Generate a QR code</Button>
+      <br/>
+      {qrLink && <Image  width={400} src={qrLink}/>}
     </div>
   )
 }
