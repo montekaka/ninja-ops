dd44418e66955f0831c0f926a2f66d974727bd43
montekaka <josh.chen.jia@gmail.com>
2021-09-20 21:57:00 -0700

    adds render the qr image

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,26 @@
 import React, {useEffect, useState} from 'react';
+import wechat from '../apis/wechat'
 
 const Home = () => {
+
+  const [qrLink, setQrLink] = useState('')
+
+  const handleGetQR = () => {
+    wechat.get(`/v1/get_tmp_qr_code_url?sceneId=1000&expire=180`)
+    .then((res) => {
+      setQrLink(res.data.code)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
   return (
-    <h1>Hello world</h1>
+    <div>
+      <h1>Create a QR code</h1>
+      <div onClick={handleGetQR}>Click</div>
+      {qrLink && <img src={qrLink}/>}
+    </div>
   )
 }
 
